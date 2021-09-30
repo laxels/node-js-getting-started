8b0c2d9cc9f235972ee5aeab44795ef84aee11c4
Axel Setyanto <laxels@gmail.com>
2021-09-29 18:49:17 -0700

    add database

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
+const { Pool } = require("pg");
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 express()
   .use(express.static(path.join(__dirname, "public")))
@@ -10,10 +17,32 @@ express()
   .post("/", postHandler)
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-function getHandler(req, res) {
-  res.send("hello world");
+async function getHandler(req, res) {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM deusx_passwords");
+
+    if (result == null || result.rows == null) {
+      res.send("Null result");
+      client.release();
+      return;
+    }
+
+    const rows = result.rows;
+    if (result.rows.length === 0) {
+      res.send("No rows found");
+      client.release();
+      return;
+    }
+
+    res.send(rows[0].hash);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error " + err);
+  }
 }
 
-function postHandler(req, res) {
+async function postHandler(req, res) {
   res.send("POST");
 }
