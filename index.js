48320f8ecb6d8767eae30e144b2d7a7dd14ef05a
Axel Setyanto <laxels@gmail.com>
2021-09-29 18:26:47 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", getHandler)
+  .post("/", postHandler)
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+function getHandler(req, res) {
+  res.send("hello world");
+}
+
+function postHandler(req, res) {
+  res.send("POST");
+}
